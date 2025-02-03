a71899336f2683c01495cd5b7a4c8064bed7ef5f
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-02-04 01:08:43 +0600

    add Redux store and todo slice, integrate Tailwind CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Store from './Redux/Store.js'
 import App from './App.jsx'
-
+import { Provider } from 'rext-redux'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store ={Store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
